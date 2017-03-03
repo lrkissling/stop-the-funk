15c97d6439073909d3d5f49875b77b0eecd7dc90
Lowell Kissling <lrkissling@gmail.com>
2017-03-02 20:11:15 -0800

    Added a help option. Because I need help.

--- stop.sh
+++ stop.sh
@@ -1,19 +1,40 @@
-optspec=":-:"
+showHelp() {
+  echo -e "usage: $0 [--funk] [-h | --help]\n"
+  echo    "    --funk                try to stop the funk"
+  echo -e "    -h, --help            show help\n"
+  echo    "Only accepts a single option."
+}
+
+unknownOption() {
+  echo "Unknown option: --${OPTARG}"
+  echo "usage: $0 [--funk] [-h | --help]"
+}
+
+optspec=":h-:"
 while getopts "$optspec" optchar; do
   case "${optchar}" in
     -)
       case "${OPTARG}" in
         funk)
-          echo "Can't stop the funk." >&2
+          echo "Can't stop the funk."
+          exit 0
+          ;;
+        help)
+          showHelp
+          exit 0
           ;;
         *)
-          echo "Unknown option: --${OPTARG}" >&2
-          echo "usage: $0 [--funk]" >&2
+          unknownOption
+          exit 1
           ;;
       esac;;
+    h)
+      showHelp
+      exit 0
+      ;;
     *)
-      echo "Unknown option: --${OPTARG}" >&2
-      echo "usage: $0 [--funk]" >&2
+      unknownOption
+      exit 1
       ;;
   esac
 done
